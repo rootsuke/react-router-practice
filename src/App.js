8f6340e211af3280a1544b391ca988fc8403d856
rootsuke <rootsuke@gmail.com>
2021-08-26 00:26:09 +0900

    add dynamic routes and nested routes

--- src/App.js
+++ src/App.js
@@ -1,19 +1,31 @@
-import { Route } from 'react-router-dom'
+import { Route, Switch, Redirect } from 'react-router-dom'
 import Welcome from './pages/Welcome';
 import Products from './pages/Products';
 import MainHeader from './components/MainHeader';
+import ProductDetail from './pages/ProductDetail';
 
 function App() {
   return (
     <div>
       <MainHeader></MainHeader>
       <main>
-        <Route path="/welcome">
-          <Welcome></Welcome>
-        </Route>
-        <Route path="/products">
-          <Products></Products>
-        </Route>
+        <Switch>
+          <Route path='/' exact>
+            <Redirect to='/welcome'></Redirect>
+          </Route>
+          
+          <Route path="/welcome">
+            <Welcome></Welcome>
+          </Route>
+          
+          <Route path="/products" exact>
+            <Products></Products>
+          </Route>
+
+          <Route path='/products/:productId'>
+            <ProductDetail></ProductDetail>
+          </Route>
+        </Switch>
       </main>
     </div>
   );
